b0d1ae552b3bc22b862e773a6dc59681889432ac
Joseph Nkeze <joe>
2021-10-23 15:11:36 -0400

    modified backends

--- backends.tf
+++ backends.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "code-sharing-bucket-jn"
     key    = "jt.tfstate"
     region = "us-east-1"
     dynamodb_table = "tfstate"
